cc9f9df5e0710f657bbf1f4f8c2f9ffedcecbc68
Yafra <yafraorg@gmail.com>
2015-02-21 23:47:04 +0100

    updated local build and docker run

--- build-local.sh
+++ build-local.sh
@@ -7,12 +7,27 @@
 # setup local environment first https://github.com/yafraorg/yafra/wiki/Development-Environment
 export BASENODE=/work/repos/git/yafra-java
 export WORKNODE=/work/yafra-runtime
+export SYSADM=/work/repos/git/yafra/org.yafra.sysadm
+export YAFRATOOLS=$SYSADM/defaults
+export YAFRABIN=$SYSADM/defaults/scripts
+export YAFRADOC=$WORKNODE/doc
+export YAFRAMAN=$WORKNODE/man
+export YAFRAEXE=$WORKNODE/bin
+    
+export PATH=$PATH:$YAFRABIN:$YAFRAEXE
 
 echo "JAVA / Maven build starting"
 echo "environment is WORKNODE = $WORKNODE - BASENODE = $BASENODE"
+test -d $WORKNODE/apps || mkdir -p $WORKNODE/apps
+test -d $WORKNODE/bin || mkdir -p $WORKNODE/bin
+
+# set database server here
+export DBSERVER=192.168.9.10
+sed -i.bup 's/localhost/'"$DBSERVER"'/' org.yafra.server.core/src/main/resources/cayenne-org_yafra.xml
 
 # maven build - build all and run some extras afterwards
 mvn install
+
 # yafra java core
 cd org.yafra.server.core/target
 cp *.jar $WORKNODE/apps
@@ -29,6 +44,32 @@ cp *.war $WORKNODE/apps
 #rcp
 cd $BASENODE/org.yafra.rcpbuild
 ./build-rcp.sh
+
+
+#
+# start yafra test first as this creates the tables if they are still missing
+echo "============================================================"
+echo " TEST CASE 1: Yafra db access with data operation and test data fill"
+echo "============================================================"
+java -jar $YAFRAEXE/serverdirectclient-1.0-jar-with-dependencies.jar
+
+echo "============================================================"
+echo " TEST CASE 2: starting server processes (JEE)"
+echo "============================================================"
+java -jar $YAFRAEXE/jee-1.0-war-exec.jar -httpPort 8081 &
+cd org.yafra.server.ejb-war
+mvn tomee:start
+cd -
+
+#
+# test yafra components
+#
+#run java tests
+echo "============================================================"
+echo " TEST CASE 3: java utils, ejb, ws"
+echo "============================================================"
+java -jar $YAFRAEXE/tests-utils-1.0-jar-with-dependencies.jar
+java -jar $YAFRAEXE/tests-ejb3-1.0-jar-with-dependencies.jar localhost
 
 
 echo "done - save in /work"
--- run-docker.sh
+++ run-docker.sh
@@ -5,13 +5,20 @@
 # variables must be set by CI service
 # setup local environment first https://github.com/yafraorg/yafra/wiki/Development-Environment
 export WORKNODE=/work/yafra-runtime
-test -d $WORKNODE/apps || mkdir -p $WORKNODE/apps
+
+cd /work/repos/yafra-java
+git pull
 
 echo "download latest build and make it available as tomcat webapp"
 # issues, tomcat users, ports and database type and config of cayenne
 cd $WORKNODE
-curl -O -u yafrarel@yafra.org:eoCvvg9J ftp://ftp.yafra.org/yafra-java-build.tar.gz
+curl -O http://www.yafra.org/build/yafra-java-build.tar.gz
+mv yafra-java-build.tar.gz /
+cd /
 tar xvfz yafra-java-build.tar.gz
 # copy to tomcat
+
+java -jar $YAFRAEXE/serverdirectclient-1.0-jar-with-dependencies.jar
+java -jar $YAFRAEXE/jee-1.0-war-exec.jar -httpPort 8081 &
 
 echo "done - running now under tomcat"
