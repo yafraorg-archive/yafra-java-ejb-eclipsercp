f9726bfb9d7a293832ff253c0e008111070d053b
yafraorg <yafraorg@gmail.com>
2015-03-18 23:06:25 +0100

    added utils junit

---
+++ org.yafra.utils/src/test/java/org/yafra/utils/LoggingTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package org.yafra.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author mwn
+ *
+ */
+public class LoggingTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#isDebugFlag()}.
+	 */
+	@Test
+	public void testIsDebugFlag() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#setDebugFlag(boolean)}.
+	 */
+	@Test
+	public void testSetDebugFlag() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#Logging()}.
+	 */
+	@Test
+	public void testLogging() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#logInfo(java.lang.String)}.
+	 */
+	@Test
+	public void testLogInfo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#logDebug(java.lang.String)}.
+	 */
+	@Test
+	public void testLogDebug() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#logError(java.lang.String, java.lang.Throwable)}.
+	 */
+	@Test
+	public void testLogError() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.yafra.utils.Logging#YafraDebug(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testYafraDebug() {
+		fail("Not yet implemented");
+	}
+
+}
